bb200f1953e008323681ff0250ca491b400a8986
marcel <marcel.taubert@epitech.eu>
2022-08-03 16:30:17 +0200

    Refactor main.rs into lib.rs to be able to use it from within the tests/*.rs files

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -143,18 +143,6 @@ pub fn _print(args: fmt::Arguments) {
 }
 
 #[test_case]
-fn test_println_simple() {
-    println!("tset_println_simple output");
-}
-
-#[test_case]
-fn test_println_many() {
-    for _ in 0..200 {
-        println!("tset_println_many output");
-    }
-}
-
-#[test_case]
 fn test_println_output() {
     let s = "Some test string that fits on a single line";
     println!("{}", s);
@@ -163,3 +151,16 @@ fn test_println_output() {
         assert_eq!(char::from(screen_char.ascii_char), c);
     }
 }
+
+#[test_case]
+pub fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+pub fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
