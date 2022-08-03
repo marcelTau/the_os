66f9930ea15a7e36540f00f342d44209b44377cf
marcel <marcel.taubert@epitech.eu>
2022-08-03 13:37:23 +0200

    add println macro

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,13 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello World!").unwrap();
-    write!(vga_buffer::WRITER.lock(), " Those are some numbers: {} -- {}", 42, 1337.0 / 69.0).unwrap();
+    println!("Hello W{}rld{}", "o", "!!!");
     loop {}
 }
