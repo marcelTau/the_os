6a5df4c75f5acdef3cc6a6ed876d5041daf22aed
marcel <marcel.taubert@epitech.eu>
2022-08-04 18:47:24 +0200

    Add bump allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -9,8 +9,41 @@ use x86_64::{
     VirtAddr,
 };
 
+use bump::BumpAllocator;
+
+pub mod bump;
+
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+fn align_up(addr: usize, align: usize) -> usize {
+    let remainder = addr % align;
+
+    if remainder == 0 {
+        addr
+    } else {
+        addr - remainder + align
+    }
+}
+
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+
+//#[global_allocator]
+//static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
