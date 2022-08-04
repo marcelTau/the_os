d7624b06a8701d85cb69c68652026777a52927a3
marcel <marcel.taubert@epitech.eu>
2022-08-04 16:01:12 +0200

    Implement BootInfoFrameAllocator to allocate new pages

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,11 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = memory::Allocator;
+    let mut frame_allocator = unsafe {
+        memory::BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
 
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    let page: Page = Page::containing_address(VirtAddr::new(0xb8000));
 
     let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
     unsafe {
--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,9 @@
 use x86_64::{
     structures::paging::{OffsetPageTable, PageTable, Page, PhysFrame, Mapper, Size4KiB, FrameAllocator},
-    VirtAddr, PhysAddr};
+    VirtAddr, PhysAddr
+};
+
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
 
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
     let l4_table = active_level_4_table(physical_memory_offset);
@@ -25,20 +28,39 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
     &mut *page_table_ptr
 }
 
-pub fn create_example_mapping(page: Page, mapper: &mut OffsetPageTable, frame_allocator: &mut impl FrameAllocator<Size4KiB>) {
-    use x86_64::structures::paging::PageTableFlags as Flags;
-
-    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
-    let flags = Flags::PRESENT | Flags::WRITABLE;
-
-    let map_to_result = unsafe {
-        mapper.map_to(page, frame, flags, frame_allocator)
-    };
-    map_to_result.expect("map_to failed").flush();
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
 }
 
-pub struct Allocator;
+impl BootInfoFrameAllocator {
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootInfoFrameAllocator {
+            memory_map,
+            next: 0,
+        }
+    }
 
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        let regions = self.memory_map.iter();
+        let usable_regions = regions.filter(|r| r.region_type == MemoryRegionType::Usable);
+        let addr_ranges = usable_regions.map(|r| r.range.start_addr()..r.range.end_addr());
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
+    }
+}
+
+
+/// Empty Test Allocator
+pub struct Allocator;
 unsafe impl FrameAllocator<Size4KiB> for Allocator {
     fn allocate_frame(&mut self) -> Option<PhysFrame> {
         None
